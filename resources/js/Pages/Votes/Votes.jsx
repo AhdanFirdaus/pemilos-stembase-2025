71bb605f1fd2c696b40d0c5ae8969bd510f7d59d
rakhaafd <rakhafausta07@gmail.com>
2025-09-16 08:22:48 +0700

    ✨ feat(ui): create user pages and color application

---
+++ resources/js/Pages/Votes/Votes.jsx
@@ -0,0 +1,11 @@
+const Votes = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold font-secondary">
+        Votes
+      </h1>
+        </div>
+    )   
+}
+
+export default Votes
