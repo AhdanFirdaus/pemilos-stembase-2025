1cf5fc0b9614bb1e7447d5f81d7b3af7a561554e
Rangki <marcelrangki@gmail.com>
2025-09-25 06:52:17 +0700

    ✨ feat(chore): add service and migration of pair candidate

---
+++ database/migrations/2025_09_24_234841_create_pair_candidates_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pair_candidates', function (Blueprint $table) {
+            $table->id();
+            // Foreign key for leader
+            $table->unsignedBigInteger('leader_id');
+            $table->foreign('leader_id')->references('id')->on('candidates')->onDelete('cascade');
+            
+            // Foreign key for co-leader
+            $table->unsignedBigInteger('co_leader_id');
+            $table->foreign('co_leader_id')->references('id')->on('candidates')->onDelete('cascade');
+            $table->string('photo_path');
+            $table->string('vision');
+            $table->string('mission');
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pair_candidates');
+    }
+};
